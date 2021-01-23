d18f6b98c699f5951f6fc7eae7f2af7f414c2747
Galen Borgman <gbrgman365@gmail.com>
2021-01-23 20:18:54 +0000

    added proper parsing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import re
+from collections import OrderedDict
 
 def write_roman(num):
 
@@ -32,10 +33,14 @@ def write_roman(num):
 client = discord.Client()
 sonnets = {}
 def build_sonnets():
-    with open("shakespeare_sonnets.txt") as sonnets:
-        all_sonnets = sonnets.read()
-    for i in range(len(re.split(all_sonnets))):
-        sonnets[write_roman(i)] = all_sonnets[i]
+    with open("shakespeare_sonnets.txt") as my_sonnets:
+        all_sonnets = my_sonnets.read()
+    roman_id = "\n\n" #"^ *M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})"
+    by_sonnet = re.split(roman_id, all_sonnets)
+    # print(len(by_sonnet))
+    # print(by_sonnet[0:3])
+    for i in range(1, len(by_sonnet), 2):
+        sonnets[write_roman((i + 1) // 2)] = f"Sonnet {write_roman((i + 1) // 2)}\n{by_sonnet[i]}"
 
         
 @client.event
@@ -49,9 +54,12 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!sonnet'):
-        await message.channel.send(sonnets[message.content.split()[1]])
-def main():
-    pass
+        await message.channel.send(get_sonnet(message.content.split()[1].upper()))
+def get_sonnet(num):
+  try:
+    return sonnets[write_roman(int(num))]
+  except:
+    return sonnets[num]
 
 if __name__ == "__main__":
     client.run(os.getenv('TOKEN'))
